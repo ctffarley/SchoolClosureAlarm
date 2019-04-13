cf5ff05f97acc243e853e6305d8265c4058627f7
Clark Farley <23282463+ctffarley@users.noreply.github.com>
2019-04-13 16:43:09 -0400

    API done, using pipenv for dependencies now

--- server/scrape.py
+++ server/scrape.py
@@ -4,16 +4,18 @@ import re
 
 ALERT = 'https://alert.umd.edu/alerts'
 
-page = requests.get(ALERT)
+def next_close():
+    page = requests.get(ALERT)
 
-soup = BeautifulSoup(page.text, 'html.parser')
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-thing = soup.find(class_ = "field-item")
-all_spans = soup.find_all('span')
-for span in all_spans:
-    try:
-        close_string = re.search("closed", span.contents[0])
-        if close_string:
-            print(re.findall(r"\w+, \w+ \d+, \d+", close_string.string))
-    except:
-        continue
+    all_spans = soup.find_all('span')
+    for span in all_spans:
+        try:
+            close_string = re.search("closed", span.contents[0])
+            if close_string:
+                close = re.findall(r"\w+ \d+, \d+", close_string.string)
+                print(close)
+                return close[0].split()
+        except:
+            continue
